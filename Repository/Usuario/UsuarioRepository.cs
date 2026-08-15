f59fdcea13f39f4bbbe90ce0e4451ba6287e39cf
Roger B <rogerbie50@gmail.com>
2026-08-15 02:10:56 -0300

    repos

---
+++ Repository/Usuario/UsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using Erp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Erp.Repository.Usuario
+{
+    
+    public class UsuarioRepository
+    {
+        private readonly AppDbContext _context;
+        public UsuarioRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+    }
+}
